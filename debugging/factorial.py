e03a2c0c0c9d23032561c4cc68c4d68120e416b3
reemmalyamani <alyamanireem@gmail.com>
2026-01-16 21:19:26 +0300

    Fix factorial script output

---
+++ debugging/factorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    print(factorial(n))
